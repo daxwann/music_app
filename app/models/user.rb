85cb29ec9936cdfccdcf03c85f78ceacf85b75a3
Dax Wann <daxwann@gmail.com>
2019-05-27 16:44:39 -0700

    add after_initialize callback in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   validates :password_digest, presence: true
   validates :email, :session_token, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6, message: "must be at least six characters" }, allow_nil: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  after_initialize :ensure_session_token
 
   def self.generate_session_token
     SecureRandom::urlsafe_base64(16)
