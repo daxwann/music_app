4c9416a68826dec721915b44412adccc464f6d3c
Dax Wann <daxwann@gmail.com>
2019-05-29 23:50:22 -0700

    add routes for albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :new, :create]
+
   resource :session, only: [:new, :create, :destroy]
-  resources :bands
+
+  resources :bands do
+    resources :albums, only: [:new]
+  end
+
+  resources :albums, only: [:show, :create, :edit, :update, :destroy]
 end
