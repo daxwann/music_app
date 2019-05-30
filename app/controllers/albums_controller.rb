57c650d83c0ee8e2dabeb1e25bc045c966a976d5
Dax Wann <daxwann@gmail.com>
2019-05-30 11:46:23 -0700

    add albums_controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,16 +1,71 @@
 class AlbumsController < ApplicationController
   def show
+    @album = Album.find_by(id: params[:id])
+    
+    if @album
+      render :show
+    else
+      redirect_to bands_url
+    end
   end
 
   def new
+    @band = Band.find_by(id: params[:band_id])
+
+    unless @band
+      redirect_to bands_url
+    end
+
+    @album = Album.new(band_id: params[:band_id])
+
+    render :new
   end
 
   def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @album = Album.find_by(id: params[:id])
+
+    if @album
+      render :edit
+    else
+      redirect_to bands_url
+    end
   end
 
   def update
+    @album = Album.find_by(id: params[:id])
+
+    unless @album
+      redirect_to bands_url
+    end
+
+    if @album.update_attributes(band_params)
+      redirect_to album_url(@album)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @album = Album.includes(:band).find_by(id: params[:id])
+    band = @album.band
+
+    @album.destroy
+    redirect_to band_url(band)
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :band_id, :year, :live)
   end
 end
